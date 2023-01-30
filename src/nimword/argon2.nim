8faebeda2195068404ea6d13e53b857ec1518750
Philipp Doerner <philippmdoerner@web.de>
2023-01-30 22:12:26 +0100

    ADD convenience functions
    It is desired to be able to store hashes in a convenient format
    that contains all necessary data to recreate the hash
    given the correct password is provided.
    
    This provides also a starting point for a further
    simplified interface where a user may *only* provide such a
    string as well as an unhashed password.

--- src/nimword/argon2.nim
+++ src/nimword/argon2.nim
@@ -1,6 +1,21 @@
 import argon2_bind
 import std/base64
 
+func hashEncode(
+  hash: string, 
+  salt: string, 
+  iterations: uint32, 
+  memoryCostK: uint32;
+  parallelism: uint32;
+  hashLen: uint32;
+  algoType: Argon2Type;
+  version: Argon2Version
+): string =
+  ## Encodes all relevant data for a password hash in a string
+  ## with the pattern "<algoType>$<version>$<iterations>$<salt>$<memoryCostK>$<parallelism>$<hashLen>$<hash>"
+  result = fmt"{algoType}${version}${iterations}${salt}${memoryCostK}${parallelism}${hashLen}${hash}"
+
+
 proc hashArgon2*(
   password: string, 
   salt: string, 
@@ -22,3 +37,15 @@ proc hashArgon2*(
 
   let res: Argon2Output = password.getOutput(salt, params)
   result = res.hash.encode()
+
+proc hashEncodeArgon2*(
+  password: string, 
+  salt: string, 
+  iterations: static uint32,
+  memoryCostK: static uint32 = Argon2DefaultParams.memoryCostK;
+  parallelism: static uint32 = Argon2DefaultParams.parallelism;
+  hashLen: static uint32 = Argon2DefaultParams.hashLen;
+  algoType: static Argon2Type = Argon2DefaultParams.algoType;
+  version: static Argon2Version = Argon2DefaultParams.version
+): string {.gcsafe, raises: [Argon2Error].} =
+  
--- src/nimword/pbkdf2.nim
+++ src/nimword/pbkdf2.nim
@@ -1,9 +1,17 @@
-from std/openssl import DLLSSLName, EVP_MD, EVP_sha256, DLLUtilName
 import std/[strformat, strutils, base64]
+from std/openssl import DLLSSLName, EVP_MD, DLLUtilName
+import ./utils
 
-proc EVP_MD_size_fixed*(md: EVP_MD): cint {.cdecl, dynlib: DLLUtilName, importc: "EVP_MD_get_size".}
-proc EVP_sha256_fixed*(): EVP_MD    {.cdecl, dynlib: DLLUtilName, importc: "EVP_sha256".}
-proc EVP_sha512_fixed*(): EVP_MD    {.cdecl, dynlib: DLLUtilName, importc: "EVP_sha512".}
+## Imports that sometimes break when importing from std/openssl - START
+proc EVP_MD_size_fixed(md: EVP_MD): cint {.cdecl, dynlib: DLLUtilName, importc: "EVP_MD_get_size".} 
+proc EVP_sha256_fixed(): EVP_MD    {.cdecl, dynlib: DLLUtilName, importc: "EVP_sha256".}
+proc EVP_sha512_fixed(): EVP_MD    {.cdecl, dynlib: DLLUtilName, importc: "EVP_sha512".}
+## Imports that sometimes break when importing from std/openssl - END
+
+func hashEncode(hash: string, salt: string, iterations: SomeInteger, algorithm: string): string =
+  ## Encodes all relevant data for a password hash in a string
+  ## with the pattern "<algorithm>$<iterations>$<salt>$<hash>"
+  result = fmt"{algorithm}${iterations}${salt}${hash}"
 
 proc PKCS5_PBKDF2_HMAC(
   pass: cstring,
@@ -63,20 +71,39 @@ proc hashPbkdf2(password: string, salt: string, iterations: int, digestFunction:
   result = encode(output)
 
 proc hashSHA256Pbkdf2*(password: string, salt: string, iterations: int): string {.gcsafe.} =
-  ## Hashes the given password with an HMAC using a SHA512 hashing function and the
-  ## PBKDF2 function to derive a key as a "hash" from the password.
+  ## Hashes the given password with an HMAC using the SHA256 hashing function 
+  ## and the PBKDF2 function to derive a key as a "hash" from the password.
   ## This is using openSSL.
   ## The returned hash is always 44 characters long.
   let digestFunction: EVP_MD = EVP_sha256_fixed()
 
   result = hashPbkdf2(password, salt, iterations, digestFunction)
 
-
 proc hashSHA512Pbkdf2*(password: string, salt: string, iterations: int): string {.gcsafe.} =
-  ## Hashes the given password with an HMAC using a SHA512 hashing function and the
-  ## PBKDF2 function to derive a key as a "hash" from the password.
+  ## Hashes the given password with an HMAC using the SHA512 hashing function 
+  ## and the PBKDF2 function to derive a key as a "hash" from the password.
   ## This is using openSSL.
   ## The returned hash is always 88 characters long.
   let digestFunction: EVP_MD = EVP_sha512_fixed()
 
   result = hashPbkdf2(password, salt, iterations, digestFunction)
+
+proc hashEncodeSHA256Pbkdf2*(password: string, salt: string, iterations: int): string {.gcsafe.} =
+  ## Hashes the given password with an HMAC using the SHA256 hashing function 
+  ## and the PBKDF2 function to derive a key as a "hash" from the password.
+  ## This is using openSSL.
+  ## The hash is returned in a string together with the algorithm, salt and 
+  ## number of iterations used to generate it, following this pattern:
+  ## "<algorithm>$<iterations>$<salt>$<hash>" 
+  let hash = hashSHA256Pbkdf2(password, salt, iterations)
+  result = hash.hashEncode(salt, iterations, "pbkdf2_sha256")
+
+proc hashEncodeSHA512Pbkdf2*(password: string, salt: string, iterations: int): string {.gcsafe.} =
+  ## Hashes the given password with an HMAC using the SHA512 hashing function 
+  ## and the PBKDF2 function to derive a key as a "hash" from the password.
+  ## This is using openSSL.
+  ## The hash is returned in a string together with the algorithm, salt and 
+  ## number of iterations used to generate it, following this pattern:
+  ## "<algorithm>$<iterations>$<salt>$<hash>" 
+  let hash = hashSHA512Pbkdf2(password, salt, iterations)
+  result = hash.hashEncode(salt, iterations, "pbkdf2_sha512")
