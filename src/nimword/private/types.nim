b792b530a1d55b19177f63c0158b9ccb8c0d04fa
Philipp Doerner <philippmdoerner@web.de>
2023-09-09 21:50:23 +0200

    Improve type docs

--- src/nimword/private/types.nim
+++ src/nimword/private/types.nim
@@ -1,5 +1,5 @@
-type Password* = distinct string
+type Password* = distinct string ## A special type for plain-text password string to prevent performing normal string operations with them. They are security critical and should not be accidentally logged or the like.
 
-converter toPassword*(str: string): Password = str.Password
+converter toPassword*(str: string): Password = str.Password ## Converter that implicitly converts any string to a `Password` type. Makes it easier to use procs with strings.
 
-type Hash* = seq[byte]
+type Hash* = seq[byte] ## A convenience type to better express hashes.
